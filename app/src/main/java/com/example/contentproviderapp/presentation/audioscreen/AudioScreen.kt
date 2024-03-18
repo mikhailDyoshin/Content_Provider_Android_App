3044d7e68bcc37efb28771cc35cf9b99b2435f7c
misha <mikhail.dyoshin@gmail.com>
2024-03-18 09:30:00 +0300

    Refactored the AudioScreen-composable
    Changed styles of a button and of a column with audio-items.

--- app/src/main/java/com/example/contentproviderapp/presentation/audioscreen/AudioScreen.kt
+++ app/src/main/java/com/example/contentproviderapp/presentation/audioscreen/AudioScreen.kt
@@ -5,15 +5,19 @@ import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.contentproviderapp.presentation.audioscreen.state.AudioState
 
 @Composable
@@ -26,7 +30,9 @@ fun AudioScreen(audios: List<AudioState>, launchActivity: (input: String) -> Uni
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Column {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.9f)) {
             audios.forEach {
                 Column(modifier = Modifier.clickable {
                     Toast.makeText(context, it.uri.toString(), Toast.LENGTH_SHORT).show()
@@ -37,11 +43,14 @@ fun AudioScreen(audios: List<AudioState>, launchActivity: (input: String) -> Uni
             }
             if (audios.isEmpty()) Text("No audios")
         }
-        Button(
+        FloatingActionButton(
             onClick = { launchActivity("audio/*") },
-            modifier = Modifier.wrapContentSize()
+            modifier = Modifier
+                .wrapContentSize()
+                .align(Alignment.End)
+                .padding(end = 20.dp, bottom = 20.dp)
         ) {
-            Text("Launch")
+            Text("+")
         }
     }
 }
